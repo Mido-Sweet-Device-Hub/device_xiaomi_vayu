071f4947dc6b7414fdff13c8c376373c864488c8
Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>
2021-09-08 10:52:49 +0000

    vayu: Initial Xtended Bringup
    Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 Project Xtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_vayu.mk
+    $(LOCAL_DIR)/xtended_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-user \
-    lineage_vayu-userdebug \
-    lineage_vayu-eng
+    xtended_vayu-user \
+    xtended_vayu-userdebug \
+    xtended_vayu-eng
--- xtended_vayu.mk
+++ xtended_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 Project Xtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,13 +8,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Xtended Stuff
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit from vayu device
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-PRODUCT_NAME := lineage_vayu
+# Xtended Stuff
+XTENDED_BUILD_TYPE := UnOfficial
+TARGET_GAPPS_ARCH := arm64
+
+# Device Identifier, This should come after all inclusions
+PRODUCT_NAME := xtended_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
